b8f103519e7ca0ae93b64d146cfbc05f048bb54c
Armin Oonk <armin.oonk@gmail.com>
2024-10-28 21:12:05 +0100

    bit of cleaning

---
+++ Leds.ino
@@ -0,0 +1,52 @@
+Adafruit_NeoPixel strip1(NUM_PIXELS_1, PIN_STRIP1, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel strip2(NUM_PIXELS_2, PIN_STRIP2, NEO_RGB + NEO_KHZ800);
+
+// ledmapping starts at the bottom
+struct led_t {
+  int strip;
+  int led;
+};
+
+Adafruit_NeoPixel ledstrips[] = {strip1, strip2};
+led_t led_mapping[NR_VERTICAL_LEDS][NR_LEDS_PER_POS];
+
+void init_led_mapping() {
+  // Setup ledstrips
+  for(int i=0; i<NR_LEDS_PER_POS; i++) {
+      ledstrips[i].begin();
+      ledstrips[i].setBrightness(128);
+  }
+  
+  for(int i=0; i<10; i++) {
+    // Define onderste strips
+    led_mapping[i][0].strip = 0;
+    led_mapping[i][0].led = 9-i;
+
+    led_mapping[i][1].strip = 0;
+    led_mapping[i][1].led = 10 + i;
+  }
+
+  for(int i=10; i<NR_VERTICAL_LEDS; i++) {
+      led_mapping[i][0].strip = 1;
+      led_mapping[i][0].led = i-10;
+
+      // Geen tweede led
+      led_mapping[i][1].strip = -1;
+      led_mapping[i][1].led = -1;
+  }
+}
+
+void set_vertical_led(int lednr, uint32_t color) {
+  for(int i=0; i<NR_LEDS_PER_POS; i++) {
+    if(led_mapping[lednr][i].strip >= 0 && led_mapping[lednr][i].strip < NR_LEDS_PER_POS) {
+      ledstrips[led_mapping[lednr][i].strip].setPixelColor(led_mapping[lednr][i].led, color);
+    }
+  }
+}
+
+void show_led_strips(int brightness) {
+  for(int i=0; i<NR_LEDS_PER_POS; i++) {
+      ledstrips[i].setBrightness(brightness);
+      ledstrips[i].show();  // send the updated pixel colors to the WS2812B hardware.
+  }
+}
--- mikky.ino
+++ mikky.ino
@@ -9,49 +9,9 @@
 #define PIN_STRIP2  4  
 #define NUM_PIXELS_2     15  // The number of LEDs (pixels) on WS2813
 
-Adafruit_NeoPixel strip1(NUM_PIXELS_1, PIN_STRIP1, NEO_RGB + NEO_KHZ800);
-Adafruit_NeoPixel strip2(NUM_PIXELS_2, PIN_STRIP2, NEO_RGB + NEO_KHZ800);
-
-// ledmapping starts at the bottom
-struct led_t {
-  int strip;
-  int led;
-};
-
 // For each vertical led upto 2 leds are possible
 #define NR_VERTICAL_LEDS 25
 #define NR_LEDS_PER_POS 2
-
-Adafruit_NeoPixel ledstrips[] = {strip1, strip2};
-led_t led_mapping[NR_VERTICAL_LEDS][NR_LEDS_PER_POS];
-
-void init_led_mapping() {
-  for(int i=0; i<10; i++) {
-    // Define onderste strips
-    led_mapping[i][0].strip = 0;
-    led_mapping[i][0].led = 9-i;
-
-    led_mapping[i][1].strip = 0;
-    led_mapping[i][1].led = 10 + i;
-  }
-
-  for(int i=10; i<NR_VERTICAL_LEDS; i++) {
-      led_mapping[i][0].strip = 1;
-      led_mapping[i][0].led = i-10;
-
-      // Geen tweede led
-      led_mapping[i][1].strip = -1;
-      led_mapping[i][1].led = -1;
-  }
-}
-
-void set_vertical_led(int lednr, uint32_t color) {
-  for(int i=0; i<NR_LEDS_PER_POS; i++) {
-    if(led_mapping[lednr][i].strip >= 0 && led_mapping[lednr][i].strip < NR_LEDS_PER_POS) {
-      ledstrips[led_mapping[lednr][i].strip].setPixelColor(led_mapping[lednr][i].led, color);
-    }
-  }
-}
 
 const uint32_t red = 0x00ff00;
 const uint32_t green = 0xff0000;
@@ -81,12 +41,6 @@ void setup() {
   
   Wire.begin();
   configureADXL345(); // Configure the sensor
-
-  strip1.begin(); // INITIALIZE WS2812B strip object (REQUIRED)
-  strip1.setBrightness(50);
-
-  strip2.begin(); // INITIALIZE WS2812B strip object (REQUIRED)
-  strip2.setBrightness(50);
 }
 
 void configureADXL345() {
@@ -156,9 +110,5 @@ void loop() {
     start_time = 0;
   }*/
   
-  strip1.setBrightness(128);
-  strip1.show();  // send the updated pixel colors to the WS2812B hardware.
-
-  strip2.setBrightness(128);
-  strip2.show();  // send the updated pixel colors to the WS2812B hardware.
+  show_led_strips(128);
 }
